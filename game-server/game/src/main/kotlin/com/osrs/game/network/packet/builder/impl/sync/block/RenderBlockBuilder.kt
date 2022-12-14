a59b95b05a772d1327ba1e116bdc03837ef898c2
Tyler Telis <xlitersps@gmail.com>
2022-12-14 17:01:42 -0500

    Typo

--- game-server/game/src/main/kotlin/com/osrs/game/network/packet/builder/impl/sync/block/RenderBlockBuilder.kt
+++ game-server/game/src/main/kotlin/com/osrs/game/network/packet/builder/impl/sync/block/RenderBlockBuilder.kt
@@ -3,6 +3,6 @@ package com.osrs.game.network.packet.builder.impl.sync.block
 import com.osrs.game.actor.Actor
 import io.ktor.utils.io.core.ByteReadPacket
 
-abstract class RenderBlockBuilder<out T : Actor>(val index: Int, val mask: Int) {
+abstract class RenderBlockBuilder<in T : Actor>(val index: Int, val mask: Int) {
     abstract fun build(actor: @UnsafeVariance T): ByteReadPacket
 }
