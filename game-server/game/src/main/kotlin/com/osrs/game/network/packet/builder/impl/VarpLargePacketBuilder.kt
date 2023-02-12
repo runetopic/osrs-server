c34a06dbdb7441f0c314de2940ac8c05cc198266
Tyler Telis <xlitersps@gmail.com>
2023-02-12 11:56:35 -0500

    Fixing Varp packets. Had Large and Small accidentally swapped.

--- game-server/game/src/main/kotlin/com/osrs/game/network/packet/builder/impl/VarpLargePacketBuilder.kt
+++ game-server/game/src/main/kotlin/com/osrs/game/network/packet/builder/impl/VarpLargePacketBuilder.kt
@@ -1,20 +1,19 @@
 package com.osrs.game.network.packet.builder.impl
 
 import com.google.inject.Singleton
-import com.osrs.common.buffer.writeByteAdd
 import com.osrs.common.buffer.writeIntLittleEndian
-import com.osrs.common.buffer.writeShortAdd
+import com.osrs.common.buffer.writeShort
 import com.osrs.game.network.packet.builder.PacketBuilder
 import com.osrs.game.network.packet.type.server.VarpLargePacket
 import java.nio.ByteBuffer
 
 @Singleton
 class VarpLargePacketBuilder : PacketBuilder<VarpLargePacket>(
-    opcode = 97,
-    size = 3
+    opcode = 26,
+    size = 6
 ) {
     override fun build(packet: VarpLargePacket, buffer: ByteBuffer) {
-        buffer.writeShortAdd(packet.id)
-        buffer.writeByteAdd(packet.value)
+        buffer.writeShort(packet.id)
+        buffer.writeIntLittleEndian(packet.value)
     }
 }
--- game-server/game/src/main/kotlin/com/osrs/game/network/packet/builder/impl/VarpSmallPacketBuilder.kt
+++ game-server/game/src/main/kotlin/com/osrs/game/network/packet/builder/impl/VarpSmallPacketBuilder.kt
@@ -1,19 +1,21 @@
 package com.osrs.game.network.packet.builder.impl
 
 import com.google.inject.Singleton
-import com.osrs.common.buffer.writeIntLittleEndian
-import com.osrs.common.buffer.writeShort
+import com.osrs.common.buffer.writeByteAdd
+import com.osrs.common.buffer.writeShortAdd
 import com.osrs.game.network.packet.builder.PacketBuilder
 import com.osrs.game.network.packet.type.server.VarpSmallPacket
 import java.nio.ByteBuffer
 
 @Singleton
 class VarpSmallPacketBuilder : PacketBuilder<VarpSmallPacket>(
-    opcode = 26,
-    size = 6
+    opcode = 97,
+    size = 3
 ) {
     override fun build(packet: VarpSmallPacket, buffer: ByteBuffer) {
-        buffer.writeShort(packet.id)
-        buffer.writeIntLittleEndian(packet.value)
+
+
+        buffer.writeShortAdd(packet.id)
+        buffer.writeByteAdd(packet.value)
     }
 }
