7b31e9ba25886d0c94dc10e9be1cba4e2c3a14eb
Jordan <personal.jordanabraham@gmail.com>
2023-03-12 17:21:51 -0400

    Add PublicChatPacket class.

--- game-server/game/src/main/kotlin/com/osrs/game/network/packet/type/client/PublicChatPacket.kt
+++ game-server/game/src/main/kotlin/com/osrs/game/network/packet/type/client/PublicChatPacket.kt
@@ -2,5 +2,11 @@ package com.osrs.game.network.packet.type.client
 
 /**
  * @author Jordan Abraham
- */class PublicChatPacket {
-}
+ */
+data class PublicChatPacket(
+    val idk: Int,
+    val color: Int,
+    val effect: Int,
+    val length: Int,
+    val bytes: ByteArray
+)
