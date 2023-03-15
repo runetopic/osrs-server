c84d22fa85b93a36429e49b73ed7deac52846d92
Jordan <personal.jordanabraham@gmail.com>
2023-03-14 20:53:44 -0400

    Adjust the array sizes for MapSquareEntry.

--- game-server/cache/src/main/kotlin/com/osrs/cache/entry/map/MapSquareEntry.kt
+++ game-server/cache/src/main/kotlin/com/osrs/cache/entry/map/MapSquareEntry.kt
@@ -6,8 +6,8 @@ data class MapSquareEntry(
     override val id: Int,
     val regionX: Int = id shr 8,
     val regionZ: Int = id and 0xFF,
-    val terrain: Array<MapSquareTerrain?> = arrayOfNulls(Short.MAX_VALUE.toInt()),
-    val locations: Array<Array<MapSquareLocation?>?> = arrayOfNulls(Short.MAX_VALUE.toInt())
+    val terrain: Array<MapSquareTerrain?> = arrayOfNulls(Short.MAX_VALUE / 2 + 1),
+    val locations: Array<Array<MapSquareLocation?>?> = arrayOfNulls(Short.MAX_VALUE / 2 + 1)
 ) : EntryType(id) {
     fun pack(level: Int, x: Int, z: Int): Int = (x and 0x3F shl 6) or (z and 0x3F) or (level shl 12)
 }
