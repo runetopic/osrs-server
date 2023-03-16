0b1122011a03fbc47924c5b3df9b1745f487e861
Tyler Telis <xlitersps@gmail.com>
2023-03-16 00:39:32 -0400

    - ExactMove.kt - ExactMoveBlockBuilder

--- game-server/game/src/main/kotlin/com/osrs/game/command/impl/TestCommand.kt
+++ game-server/game/src/main/kotlin/com/osrs/game/command/impl/TestCommand.kt
@@ -1,10 +1,12 @@
 package com.osrs.game.command.impl
 
 import com.google.inject.Singleton
+import com.osrs.common.map.location.transform
 import com.osrs.game.actor.hit.HealthBar.Companion.Default
 import com.osrs.game.actor.hit.HitSplat
 import com.osrs.game.actor.hit.HitType.Companion.Normal
 import com.osrs.game.actor.player.Player
+import com.osrs.game.actor.render.type.ExactMove
 import com.osrs.game.actor.render.type.HealthUpdate
 import com.osrs.game.command.CommandListener
 
@@ -45,6 +47,15 @@ class TestCommand : CommandListener(
                 bars = arrayOf(
                     Default
                 )
+            )
+        )
+
+        player.renderer.update(
+            ExactMove(
+                currentLocation = player.location,
+                firstLocation = player.location.transform(0, -5, 0),
+                firstDuration = 20,
+                angle = 512
             )
         )
     }
