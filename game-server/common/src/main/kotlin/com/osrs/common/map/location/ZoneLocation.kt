73f3a1de2210d06b56a45a31d0dc00bc144ed122
Jordan <personal.jordanabraham@gmail.com>
2023-03-19 18:34:45 -0400

    Remove inline from properties.

--- game-server/common/src/main/kotlin/com/osrs/common/map/location/ZoneLocation.kt
+++ game-server/common/src/main/kotlin/com/osrs/common/map/location/ZoneLocation.kt
@@ -5,14 +5,14 @@ value class ZoneLocation(
     val packedLocation: Int
 ) {
     constructor(x: Int, z: Int, level: Int = 0) : this((x and 0x7ff) or ((z and 0x7ff) shl 11) or ((level and 0x3) shl 22))
-    inline val id get() = x or (z shl 11) or (level shl 22)
-    inline val level get() = (packedLocation shr 22) and 0x3
-    inline val z get() = (packedLocation shr 11) and 0x7ff
-    inline val x get() = packedLocation and 0x7ff
-    inline val location get() = Location(x shl 3, z shl 3, level)
-    inline val regionId get() = z + (x shl 8)
-    inline val regionX get() = x shr 3
-    inline val regionZ get() = z shr 3
+    val id get() = x or (z shl 11) or (level shl 22)
+    val level get() = (packedLocation shr 22) and 0x3
+    val z get() = (packedLocation shr 11) and 0x7ff
+    val x get() = packedLocation and 0x7ff
+    val location get() = Location(x shl 3, z shl 3, level)
+    val regionId get() = z + (x shl 8)
+    val regionX get() = x shr 3
+    val regionZ get() = z shr 3
 
     override fun toString(): String = "ZoneLocation($x, $z, $level)"
 }
