63a7071ff1ce30cea89e2cb913a4d82a07048b0c
Jordan <personal.jordanabraham@gmail.com>
2023-03-19 05:36:15 -0400

    Move location property to constructor.

--- game-server/game/src/main/kotlin/com/osrs/game/actor/npc/NPC.kt
+++ game-server/game/src/main/kotlin/com/osrs/game/actor/npc/NPC.kt
@@ -1,12 +1,15 @@
 package com.osrs.game.actor.npc
 
+import com.osrs.common.map.location.Location
 import com.osrs.game.actor.Actor
 import com.osrs.game.actor.movement.MoveDirection
 import com.osrs.game.world.World
 
 class NPC(
-    world: World
-) : Actor(world) {
+    val id: Int,
+    world: World,
+    location: Location
+) : Actor(world, location) {
     override var moveDirection: MoveDirection? = null
 
     override fun login() {
