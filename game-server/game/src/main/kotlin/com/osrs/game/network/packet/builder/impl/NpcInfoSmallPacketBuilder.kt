8705a501dabd3a92e9a6718d845698f8f0e23649
Jordan <personal.jordanabraham@gmail.com>
2023-03-19 05:57:13 -0400

    Add NpcInfoSmallPacketBuilder.

--- game-server/game/src/main/kotlin/com/osrs/game/network/packet/builder/impl/NpcInfoSmallPacketBuilder.kt
+++ game-server/game/src/main/kotlin/com/osrs/game/network/packet/builder/impl/NpcInfoSmallPacketBuilder.kt
@@ -3,11 +3,17 @@ package com.osrs.game.network.packet.builder.impl
 import com.google.inject.Singleton
 import com.osrs.game.network.packet.builder.PacketBuilder
 import com.osrs.game.network.packet.type.server.NpcInfoPacket
+import java.nio.ByteBuffer
 
 /**
  * @author Jordan Abraham
  */
 @Singleton
-class NpcInfoPacketBuilder : PacketBuilder<NpcInfoPacket>(
-    opcode =
-)
+class NpcInfoSmallPacketBuilder : PacketBuilder<NpcInfoPacket>(
+    opcode = 9,
+    size = -2
+) {
+    override fun build(packet: NpcInfoPacket, buffer: ByteBuffer) {
+        TODO("Not yet implemented")
+    }
+}
