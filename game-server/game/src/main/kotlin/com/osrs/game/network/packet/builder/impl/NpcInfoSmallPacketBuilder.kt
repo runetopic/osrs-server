807c029fadc8f6e58a48fd552383995c3365833d
Jordan <personal.jordanabraham@gmail.com>
2023-03-19 05:57:01 -0400

    Add NpcInfoPacket.

---
+++ game-server/game/src/main/kotlin/com/osrs/game/network/packet/builder/impl/NpcInfoSmallPacketBuilder.kt
@@ -0,0 +1,13 @@
+package com.osrs.game.network.packet.builder.impl
+
+import com.google.inject.Singleton
+import com.osrs.game.network.packet.builder.PacketBuilder
+import com.osrs.game.network.packet.type.server.NpcInfoPacket
+
+/**
+ * @author Jordan Abraham
+ */
+@Singleton
+class NpcInfoPacketBuilder : PacketBuilder<NpcInfoPacket>(
+    opcode =
+)
---
+++ game-server/game/src/main/kotlin/com/osrs/game/network/packet/type/server/NpcInfoPacket.kt
@@ -0,0 +1,12 @@
+package com.osrs.game.network.packet.type.server
+
+import com.osrs.game.actor.player.Viewport
+import com.osrs.game.network.packet.Packet
+
+/**
+ * @author Jordan Abraham
+ */
+data class NpcInfoPacket(
+    val viewport: Viewport,
+    val highDefinitionUpdates: Array<ByteArray?>
+) : Packet
