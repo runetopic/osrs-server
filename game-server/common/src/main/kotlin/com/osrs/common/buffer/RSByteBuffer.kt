2c6b393c52387d5c583ac4b520375f04b1059fcd
Jordan <personal.jordanabraham@gmail.com>
2023-03-20 23:34:48 -0400

    Update RSByteBuffer.

--- game-server/common/src/main/kotlin/com/osrs/common/buffer/RSByteBuffer.kt
+++ game-server/common/src/main/kotlin/com/osrs/common/buffer/RSByteBuffer.kt
@@ -12,49 +12,120 @@ class RSByteBuffer(
 
     private var accessBitsIndex = -1
 
-    fun readByte(): Int = buffer.get().toInt()
-    fun readUByte(): Int = buffer.get().toInt() and 0xFF
-    fun readUByteSubtract(): Int = readUByte() - 128 and 0xFF
-    fun readUByteAdd(): Int = readUByte() + 128 and 0xFF
-    fun readUByteNegate(): Int = -readUByte() and 0xFF
+    fun readByte(): Int {
+        checkAccessingBytes()
+        return buffer.get().toInt()
+    }
 
-    fun readShort(): Int = buffer.short.toInt()
-    fun readUShort(): Int = buffer.short.toInt() and 0xFFFF
-    fun readUShortAdd(): Int = (readUByte() shl 8) or readUByteAdd()
-    fun readUShortSubtract(): Int = (readUByte() shl 8) or readUByteSubtract()
-    fun readUShortLittleEndian(): Int = readUByte() or (readUByte() shl 8)
-    fun readUShortLittleEndianSubtract(): Int = readUByteSubtract() or (readUByte() shl 8)
-    fun readUShortLittleEndianAdd(): Int = readUByteAdd() or (readUByte() shl 8)
-    fun readU24BitInt(): Int = (readUByte() shl 16) or readUShort()
+    fun readUByte(): Int {
+        checkAccessingBytes()
+        return buffer.get().toInt() and 0xFF
+    }
 
-    fun readInt(): Int = buffer.int
-    fun readIntLittleEndian(): Int = readUShortLittleEndian() or (readUByte() shl 16) or (readUByte() shl 24)
-    fun readIntV1(): Int = readUShort() or (readUByte() shl 24) or (readUByte() shl 16)
-    fun readIntV2(): Int = (readUByte() shl 16) or (readUByte() shl 24) or readUShortLittleEndian()
+    fun readUByteSubtract(): Int {
+        checkAccessingBytes()
+        return ((buffer.get().toInt() and 0xFF) - 128) and 0xFF
+    }
 
-    fun readLong(): Long = buffer.long
+    fun readUByteAdd(): Int {
+        checkAccessingBytes()
+        return ((buffer.get().toInt() and 0xFF) + 128) and 0xFF
+    }
 
-    tailrec fun readIncrSmallSmart(increment: Int = readUShortSmart(), offset: Int = 0): Int {
+    fun readUByteNegate(): Int {
+        checkAccessingBytes()
+        return -(buffer.get().toInt() and 0xFF) and 0xFF
+    }
+
+    fun readShort(): Int {
+        checkAccessingBytes()
+        return buffer.short.toInt()
+    }
+
+    fun readUShort(): Int {
+        checkAccessingBytes()
+        return buffer.short.toInt() and 0xFFFF
+    }
+
+    fun readUShortAdd(): Int {
+        checkAccessingBytes()
+        return ((buffer.get().toInt() and 0xFF) shl 8) or (((buffer.get().toInt() and 0xFF) + 128) and 0xFF)
+    }
+
+    fun readUShortSubtract(): Int {
+        checkAccessingBytes()
+        return ((buffer.get().toInt() and 0xFF) shl 8) or (((buffer.get().toInt() and 0xFF) - 128) and 0xFF)
+    }
+
+    fun readUShortLittleEndian(): Int {
+        checkAccessingBytes()
+        return (buffer.get().toInt() and 0xFF) or ((buffer.get().toInt() and 0xFF) shl 8)
+    }
+
+    fun readUShortLittleEndianSubtract(): Int {
+        checkAccessingBytes()
+        return (((buffer.get().toInt() and 0xFF) - 128) and 0xFF) or ((buffer.get().toInt() and 0xFF) shl 8)
+    }
+
+    fun readUShortLittleEndianAdd(): Int {
+        checkAccessingBytes()
+        return (((buffer.get().toInt() and 0xFF) + 128) and 0xFF) or ((buffer.get().toInt() and 0xFF) shl 8)
+    }
+
+    fun readU24BitInt(): Int {
+        checkAccessingBytes()
+        return ((buffer.get().toInt() and 0xFF) shl 16) or (buffer.short.toInt() and 0xFFFF)
+    }
+
+    fun readInt(): Int {
+        checkAccessingBytes()
+        return buffer.int
+    }
+
+    fun readIntLittleEndian(): Int {
+        checkAccessingBytes()
+        return ((buffer.get().toInt() and 0xFF) or ((buffer.get().toInt() and 0xFF) shl 8)) or ((buffer.get().toInt() and 0xFF) shl 16) or ((buffer.get().toInt() and 0xFF) shl 24)
+    }
+
+    fun readIntV1(): Int {
+        checkAccessingBytes()
+        return (buffer.short.toInt() and 0xFFFF) or ((buffer.get().toInt() and 0xFF) shl 24) or ((buffer.get().toInt() and 0xFF) shl 16)
+    }
+
+    fun readIntV2(): Int {
+        checkAccessingBytes()
+        return ((buffer.get().toInt() and 0xFF) shl 16) or ((buffer.get().toInt() and 0xFF) shl 24) or ((buffer.get().toInt() and 0xFF) or ((buffer.get().toInt() and 0xFF) shl 8))
+    }
+
+    fun readLong(): Long {
+        checkAccessingBytes()
+        return buffer.long
+    }
+
+    tailrec fun readIncrSmallSmart(ensure: Unit = checkAccessingBytes(), increment: Int = readUShortSmart(), offset: Int = 0): Int {
         if (increment != Short.MAX_VALUE.toInt()) return offset + increment
-        return readIncrSmallSmart(offset = offset + Short.MAX_VALUE)
+        return readIncrSmallSmart(ensure = ensure, offset = offset + Short.MAX_VALUE)
     }
 
     fun readShortSmart(): Int {
-        val peek = readUByte()
-        return if (peek < 128) peek - 64 else (peek shl 8 or readUByte()) - 49152
+        checkAccessingBytes()
+        val peek = buffer.get().toInt() and 0xFF
+        return if (peek < 128) peek - 64 else (peek shl 8 or (buffer.get().toInt() and 0xFF)) - 49152
     }
 
     fun readUShortSmart(): Int {
-        val peek = readUByte()
-        return if (peek < 128) peek else (peek shl 8 or readUByte()) - 32768
+        checkAccessingBytes()
+        val peek = buffer.get().toInt() and 0xFF
+        return if (peek < 128) peek else (peek shl 8 or (buffer.get().toInt() and 0xFF)) - 32768
     }
 
     fun readUIntSmart(): Int {
-        val peek = readUByte()
+        checkAccessingBytes()
+        val peek = buffer.get().toInt() and 0xFF
         return if (peek < 0) {
-            ((peek shl 24) or (readUByte() shl 16) or (readUByte() shl 8) or readUByte()) and Integer.MAX_VALUE
+            ((peek shl 24) or ((buffer.get().toInt() and 0xFF) shl 16) or ((buffer.get().toInt() and 0xFF) shl 8) or (buffer.get().toInt() and 0xFF)) and Integer.MAX_VALUE
         } else {
-            (peek shl 8 or readUByte()).toShort().let { if (it == Short.MAX_VALUE) -1 else it }.toInt()
+            (peek shl 8 or (buffer.get().toInt() and 0xFF)).toShort().let { if (it == Short.MAX_VALUE) -1 else it }.toInt()
         }
     }
 
@@ -68,177 +139,146 @@ class RSByteBuffer(
         return method7754(opcode, offset = offset + 7, value = value or ((opcode and 127) shl offset))
     }
 
-    fun readStringCp1252NullTerminated(): String = String(readUChars(lengthToDelimiter(0))).also {
-        discard(1)
+    fun readStringCp1252NullTerminated(): String {
+        checkAccessingBytes()
+        return String(readUChars(lengthUntilDelimiter(0))).also {
+            discard(1)
+        }
     }
 
     fun readStringCp1252NullCircumfixed(): String {
+        checkAccessingBytes()
         if (readByte() != 0) throw IllegalArgumentException()
         return readStringCp1252NullTerminated()
     }
 
-    fun readUChars(n: Int): CharArray = CharArray(n) { readUByte().toChar() }
+    fun readUChars(n: Int): CharArray {
+        checkAccessingBytes()
+        return CharArray(n) { (buffer.get().toInt() and 0xFF).toChar() }
+    }
 
     fun writeByte(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         buffer.put(value.toByte())
     }
 
     fun writeByteSubtract(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         buffer.put((128 - value.toByte()).toByte())
     }
 
     fun writeByteAdd(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         buffer.put((value.toByte() + 128).toByte())
     }
 
     fun writeByteNegate(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         buffer.put((-value.toByte()).toByte())
     }
 
     fun writeShort(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         buffer.putShort(value.toShort())
     }
 
     fun writeShortAdd(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         buffer.put((value shr 8).toByte())
-        writeByteAdd(value)
+        buffer.put((value.toByte() + 128).toByte())
     }
 
     fun writeShortLittleEndian(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         buffer.put(value.toByte())
         buffer.put((value shr 8).toByte())
     }
 
     fun writeShortLittleEndianAdd(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
-        writeByteAdd(value)
+        checkAccessingBytes()
+        buffer.put((value.toByte() + 128).toByte())
         buffer.put((value shr 8).toByte())
     }
 
     fun write24BitInt(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         buffer.put((value shr 16).toByte())
         buffer.putShort(value.toShort())
     }
 
     fun writeInt(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         buffer.putInt(value)
     }
 
     fun writeIntLittleEndian(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
-        writeShortLittleEndian(value)
+        checkAccessingBytes()
+        buffer.put(value.toByte())
+        buffer.put((value shr 8).toByte())
         buffer.put((value shr 16).toByte())
         buffer.put((value shr 24).toByte())
     }
 
     fun writeIntV1(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         buffer.putShort(value.toShort())
         buffer.put((value shr 24).toByte())
         buffer.put((value shr 16).toByte())
     }
 
     fun writeIntV2(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         buffer.put((value shr 16).toByte())
         buffer.put((value shr 24).toByte())
-        writeShortLittleEndian(value)
+        buffer.put(value.toByte())
+        buffer.put((value shr 8).toByte())
     }
 
     fun writeLong(value: Long) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         buffer.putLong(value)
     }
 
     fun writeBytes(bytes: ByteArray) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         for (byte in bytes) {
-            writeByte(byte.toInt())
+            buffer.put(byte)
         }
     }
 
     fun writeBytesAdd(bytes: ByteArray) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         for (byte in bytes) {
-            writeByteAdd(byte.toInt())
+            buffer.put((byte + 128).toByte())
         }
     }
 
     fun writeReversedAdd(bytes: ByteArray) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         for (byte in bytes.reversedArray()) {
-            writeByteAdd(byte.toInt())
+            buffer.put((byte + 128).toByte())
         }
     }
 
     fun writeSmartByteShort(value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         if (value in 0..127) {
-            writeByte(value)
+            buffer.put(value.toByte())
         } else {
-            writeShort(value + 32768)
+            buffer.putShort((value + 32768).toShort())
         }
     }
 
     fun writeStringCp1252NullTerminated(value: String) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         for (byte in value.toByteArray()) {
-            writeByte(byte.toInt())
+            buffer.put(byte)
         }
         writeByte(0)
     }
 
     fun fill(amount: Int, value: Int) {
-        if (!ensureBytes()) {
-            throw IllegalAccessException("Buffer must be in bytes access.")
-        }
+        checkAccessingBytes()
         for (index in 0 until amount) {
             buffer.put(value.toByte())
         }
@@ -248,7 +288,7 @@ class RSByteBuffer(
         buffer.position(buffer.position() + amount)
     }
 
-    fun lengthToDelimiter(delimiter: Int): Int {
+    fun lengthUntilDelimiter(delimiter: Int): Int {
         var count = 0
         while (buffer[buffer.position() + count].toInt() != delimiter) {
             count++
@@ -271,9 +311,7 @@ class RSByteBuffer(
     fun array(): ByteArray = buffer.array()
 
     fun bitAccess(block: () -> Unit) {
-        if (ensureBits()) {
-            throw IllegalAccessException("Buffer is already accessing bits.")
-        }
+        checkAccessingBytes()
         accessBitsIndex = buffer.position() * 8
         block.invoke()
         buffer.position((accessBitsIndex + 7) / 8)
@@ -281,9 +319,8 @@ class RSByteBuffer(
     }
 
     fun writeBits(count: Int, value: Int) {
-        if (!ensureBits()) {
-            throw IllegalAccessException("Buffer must be in bits access.")
-        }
+        checkAccessingBits()
+
         var remainingBits = count
         var byteIndex = accessBitsIndex shr 3
         var bitIndex = 8 - (accessBitsIndex and 7)
@@ -314,6 +351,13 @@ class RSByteBuffer(
         }
     }
 
-    private fun ensureBytes(): Boolean = accessBitsIndex == -1
-    private fun ensureBits(): Boolean = accessBitsIndex != -1
+    private fun checkAccessingBytes() {
+        if (accessBitsIndex == -1) return
+        throw IllegalAccessException("Buffer must be in bytes access.")
+    }
+
+    private fun checkAccessingBits() {
+        if (accessBitsIndex != -1) return
+        throw IllegalAccessException("Buffer must be in bits access.")
+    }
 }
